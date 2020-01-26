b3640f826911910638452d6c06dbfb54b78d2f9b
Ahmad Jarara (MBP) <ajarara94@gmail.com>
2020-01-26 17:24:33 -0500

    renames

--- src/main/java/Heap.java
+++ src/main/java/Heap.java
@@ -2,36 +2,36 @@ import java.util.*;
 
 public final class Heap<T> {
 
-    private final List<T> backing;
+    private final List<T> list;
     private final Comparator<T> comparator;
 
     public Heap(Comparator<T> comparator) {
         this(comparator, new ArrayList<>());
     }
 
-    private Heap(Comparator<T> comparator, List<T> backing) {
-        this.backing = backing;
+    private Heap(Comparator<T> comparator, List<T> list) {
+        this.list = list;
         this.comparator = comparator;
     }
 
     public static <T> void inPlaceSort(List<T> things, Comparator<T> comparator) {
         Heap<T> heap = new Heap<>(comparator, things);
         heap.heapify();
-        int heapThreshold = heap.backing.size() - 1;
+        int heapThreshold = heap.list.size() - 1;
         while (heapThreshold >= 0) {
-            heap.backing.add(heap.pop(heapThreshold));
+            heap.list.add(heap.pop(heapThreshold));
             heapThreshold -= 1;
         }
     }
 
     public void add(T element) {
-        backing.add(element);
-        int currIdx = backing.size() - 1;
+        list.add(element);
+        int currIdx = list.size() - 1;
         if (currIdx == 0) {
             return;
         }
         int parentIdx = (currIdx - 1) / 2;
-        while (obeysComparator(currIdx, parentIdx)) {
+        while (compares(parentIdx, currIdx)) {
             swap(currIdx, parentIdx);
             if (parentIdx != 0) {
                 currIdx = parentIdx;
@@ -43,22 +43,22 @@ public final class Heap<T> {
     }
 
     public T remove() {
-        int size = backing.size();
+        int size = list.size();
         if (size == 0) {
             throw new IllegalStateException("Remove called on an empty heap!");
         } else if (size == 1) {
-            return backing.remove(0);
+            return list.remove(0);
         } else {
             return pop(size - 1);
         }
     }
 
     public int size() {
-        return backing.size();
+        return list.size();
     }
 
     private void heapify() {
-        int heapSize = backing.size();
+        int heapSize = list.size();
         int heapThreshold = heapSize / 2;
 
         while (heapThreshold >= 0) {
@@ -69,7 +69,7 @@ public final class Heap<T> {
 
     private T pop(int heapBound) {
         swap(0, heapBound);
-        T returned = backing.remove(heapBound);
+        T returned = list.remove(heapBound);
         siftDown(0, heapBound);
         return returned;
     }
@@ -87,9 +87,9 @@ public final class Heap<T> {
     }
 
     private void swap(int idxA, int idxB) {
-        T temp = backing.get(idxA);
-        backing.set(idxA, backing.get(idxB));
-        backing.set(idxB, temp);
+        T temp = list.get(idxA);
+        list.set(idxA, list.get(idxB));
+        list.set(idxB, temp);
     }
 
     private OptionalInt getSwapIndex(int parentIdx, int heapBound) {
@@ -97,23 +97,22 @@ public final class Heap<T> {
         int rightIdx = 2 * parentIdx + 2;
 
         if (leftIdx < heapBound) {
-            if (obeysComparator(leftIdx, parentIdx)) {
-                if (rightIdx < heapBound && obeysComparator(rightIdx, leftIdx)) {
+            if (compares(parentIdx, leftIdx)) {
+                if (rightIdx < heapBound && compares(leftIdx, rightIdx)) {
                     return OptionalInt.of(rightIdx);
                 }
                 return OptionalInt.of(leftIdx);
-            } else if (rightIdx < heapBound && obeysComparator(rightIdx, parentIdx)) {
+            } else if (rightIdx < heapBound && compares(parentIdx, rightIdx)) {
                 return OptionalInt.of(rightIdx);
             }
         }
         return OptionalInt.empty();
     }
 
-    private boolean obeysComparator(int greaterIdx, int lesserIdx) {
-        boolean toBeReturned = comparator.compare(
-                backing.get(greaterIdx),
-                backing.get(lesserIdx)
-        ) < 0;
-        return toBeReturned;
+    private boolean compares(int greaterIdx, int lesserIdx) {
+        return comparator.compare(
+                list.get(greaterIdx),
+                list.get(lesserIdx)
+        ) > 0;
     }
 }
--- src/test/kotlin/HeapTest.kt
+++ src/test/kotlin/HeapTest.kt
@@ -1,4 +1,3 @@
-import org.junit.Ignore
 import org.junit.Test
 import java.util.*
 import kotlin.Comparator
@@ -7,7 +6,7 @@ import kotlin.random.Random
 class HeapTest {
 
     @Test
-    fun `a priority queue reversed behaves like our heap`() {
+    fun `a priority queue behaves like our heap`() {
         val intComparison = Comparator<Int> { x, y -> x.compareTo(y) }
         assertPrioritySortEquivalence(listOf(1, 3, 5), intComparison)
         assertPrioritySortEquivalence(listOf(-2, 5, 66, 1800, -532, -686), intComparison)
@@ -41,7 +40,6 @@ class HeapTest {
     fun `an in order heap sort sorts`() {
         val ourList = mutableListOf(-2, 5, 66, 1800, -532, -686)
         val timSort = ourList.sorted()
-            // .reversed()
 
         Heap.inPlaceSort(ourList) { x, y -> x.compareTo(y) }
 
